07e84594f763a102c0a724721c743901a33c7137
KChheda <kekin.chheda@davita.com>
2022-03-23 11:56:28 +0530

    Code cleanup

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const formidable = require('formidable');
 const dotenv = require('dotenv');
 const users = require('./models/users');
 const jwt = require('jsonwebtoken');
 const lib = require('./lib');
-// const cors = require('./cors');
 
-// const form  = formidable({multiples: true});
 dotenv.config();
 
 const app = express();
@@ -17,25 +13,12 @@ app.use(express.json(), (req, res, next) => {
     next();
 });
 
-// app.use(formidable());
-
 
 const port  = process.env.PORT || 8080;
 const secretKey = process.env.SECRET_KEY;
 
 app.post('/login', async (req, res) => {
     const {email, password} = req.body;
-    // console.log(req.fields);
-    // const {} = req.fields;
-
-    // console.log('req.params', req);
-    // form.parse(req, (err, fields, files) => {
-    //     console.log('fields',  Object.keys(fields));
-    // });
-
-    console.log('Email', email);
-    console.log('Password', password);
-
     const isUserPresent = users.findIndex(u => u.email===email && u.password === password);
 
     if(isUserPresent > -1) {
